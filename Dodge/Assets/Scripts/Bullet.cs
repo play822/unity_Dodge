36f95c648de0b84981177ce67c3cc618077c35c2
DESKTOP-R8VPL6D\qjawl <tjqjawls2@gmail.com>
2023-03-20 20:53:43 +0900

    완성

---
+++ Dodge/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlyerController playerController = other.GetComponent<PlyerController>();
+
+            if (playerController != null)
+                playerController.Die();
+        }
+    }
+}
---
+++ Dodge/Assets/Scripts/PlyerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerController : MonoBehaviour
+{
+    private Rigidbody playerRigibody;
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigibody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+
+        playerRigibody.velocity = newVelocity;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+
+        GameManger gameManger = FindObjectOfType<GameManger>();
+        gameManger.EndGame();
+    }
+}
